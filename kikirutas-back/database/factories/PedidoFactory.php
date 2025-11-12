6c6844949b6d8b8b1fa1e1d3e7fffa646f390540
Abraham Gutiérrez <habrahamgutierrez289@gmail.com>
2025-11-12 00:02:52 -0500

    se añadio pedido en factories para la base de datos

--- kikirutas-back/database/factories/PedidoFactory.php
+++ kikirutas-back/database/factories/PedidoFactory.php
@@ -4,20 +4,19 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Pedido>
- */
 class PedidoFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'producto' => $this->faker->randomElement(['Alimento A-20 kg','Alimento B-40 kg','Maíz 50 kg']),
+            'cantidad' => $this->faker->numberBetween(1,8),
+            'estado'   => 'pendiente',
+            'fecha'    => now(),
+            'solicitante_nombre'     => $this->faker->name(),
+            'solicitante_comunidad'  => $this->faker->randomElement(['Kancabchén','Dziuché','Sacalaca']),
+            'telefono' => $this->faker->numerify('983########'),
+            'notas'    => $this->faker->sentence(),
         ];
     }
 }
